8b1575229bbde692bd5cb919dc1c4aca4b2e83fd
Bruno Capella <capella.bruno@gmail.com>
2016-01-14 18:05:14 -0200

    start developer game.

---
+++ GameScrumMaster/Game.Domain/Pessoa.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Domain
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public TipoPessoa Tipo { get; set; }
+    }
+}
